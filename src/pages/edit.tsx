034363fed93b52fce9299de9bda6c4b366f0959b
Chetan Verma <chetan61verma@gmail.com>
2022-07-15 02:44:26 +0530

    Generating Selection State Based on Value

--- src/pages/edit.tsx
+++ src/pages/edit.tsx
@@ -1,8 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { TODOType } from "../@types";
 import Header from "../components/Header";
 import Layout from "../components/Layout";
 
 const Edit = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [badge, setBadge] = useState("Todo");
+
+  const generateOptions = (badge: String) => {
+    switch (badge) {
+      case "Todo":
+        return (
+          <>
+            <option value={TODOType["INPROGRESS"]}>InProgress</option>
+            <option value={TODOType["TODO"]}>Todo</option>
+          </>
+        );
+      case "Blocked":
+        return (
+          <>
+            <option value={TODOType["TODO"]}>Todo</option>
+            <option value={TODOType["BLOCKED"]}>Blocked</option>
+          </>
+        );
+      case "InProgress":
+        return (
+          <>
+            <option value={TODOType["BLOCKED"]}>Blocked</option>
+            <option value={TODOType["INQA"]}>InQA</option>
+            <option value={TODOType["INPROGRESS"]}>InProgress</option>
+          </>
+        );
+      case "InQA":
+        return (
+          <>
+            <option value={TODOType["TODO"]}>Todo</option>
+            <option value={TODOType["DONE"]}>Done</option>
+            <option value={TODOType["INQA"]}>InQA</option>
+          </>
+        );
+      case "Done":
+        return (
+          <>
+            <option value={TODOType["DEPLOYED"]}>Deployed</option>
+            <option value={TODOType["DONE"]}>Done</option>
+          </>
+        );
+      case "Deployed":
+        return (
+          <>
+            <option value={TODOType["DEPLOYED"]}>Deployed</option>
+          </>
+        );
+    }
+  };
+  useEffect(() => {}, []);
   return (
     <Layout>
       <Header location={"Edit"}></Header>
@@ -12,12 +65,19 @@ const Edit = () => {
           <input
             type="text"
             className="w-full mt-5 p-4 border-b border-b-slate-600 bg-slate-200"
-            placeholder="Title"></input>
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}></input>
           <textarea
             className="w-full h-96 mt-5 p-4 border-b border-b-slate-600 bg-slate-200"
-            placeholder="Description"></textarea>
-          <select className="w-full h-16 p-4 border-b-2 border-b-slate-600 bg-slate-200 mt-5">
-            <option className="h-16">InQA</option>
+            placeholder="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}></textarea>
+          <select
+            onChange={(e) => setBadge(e.target.value)}
+            value={badge}
+            className="w-full h-16 p-4 border-b-2 border-b-slate-600 bg-slate-200 mt-5">
+            {generateOptions(badge)}
           </select>
         </div>
         <div className="w-full flex items-center my-10">
